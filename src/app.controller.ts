231ca52a07146b0c437f2b024c22bcfb09b4390a
collinsmezie <collinschimezieobinwa@gmail.com>
2025-03-30 16:16:34 +0200

    Refactor app.controller.ts and add default route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,19 @@
 import { Controller, Request, Post, Get, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private appService: AppService
+
+  ) {}
+
+  // get default route
+  @Get('home')
+  getHello(): string {
+    return this.appService.getHello();
+  }
 }
